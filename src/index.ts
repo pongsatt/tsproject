1b393231f7ec860439e44138195c4f3ab26d9147
ponggit <pongsatt@gmail.com>
2018-04-30 13:16:20 +0700

    Add graphql server and schemas generation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hi there')
+import { GraphQLServer } from 'graphql-yoga'
+import { GQLResolver, GQLProduct } from './schemas.d';
+import { Props } from 'graphql-yoga/dist/src/types';
+const typeDefs = 'src/schemas.graphql'
+
+const resolvers = {
+  Query: {
+    product: async (_, {id}) => {
+      return {
+        id: new Date().getTime().toString(),
+        name: 'test 1',
+        createdDate: new Date()
+      }
+    }
+  }
+} as GQLResolver
+
+const server = new GraphQLServer({ typeDefs, resolvers } as Props)
+server.start(() => console.log('Server is running on http://localhost:4000'))
